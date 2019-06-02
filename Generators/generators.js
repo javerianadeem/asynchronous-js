ff7f3aff0b59f673a6f00f1f2a98f475360b9706
Javeria <javerianadeem03@gmail.com>
2019-06-02 23:19:52 +0500

    passing and retrieving data from generators

--- Generators/generators.js
+++ Generators/generators.js
@@ -1,9 +1,32 @@
 window.onload = function(){
-    function* gen()
-    {
-        var x = yield 10;
-    }
-    var myGen = gen();
-    console.log(myGen.next())
-    console.log(myGen.next())
+    // function* gen()
+    // {
+    //     var x = yield 10;
+    //     console.log(x)
+    // }
+    // var myGen = gen();
+    // console.log(myGen.next())
+    // console.log(myGen.next(20))
+
+    genWrap(function*(){
+        var tweets = yield $.get("data/tweets.json");
+        console.log(tweets);
+        
+        var friends = yield $.get("data/friends.json");
+        console.log(friends);
+       
+        var videos = yield $.get("data/videos.json");
+        console.log(videos);
+     });
+     function genWrap(generator){
+       var gen = generator();
+       function handle(yielded){
+         if(!yielded.done){
+           yielded.value.then(function(data){
+             return handle(gen.next(data));
+           });
+         }
+       }
+       return handle(gen.next());
+     }
 }
