9c60840bc96f91d21b3dc969ec798c475ca2eb5a
Javeria <javerianadeem03@gmail.com>
2019-06-02 22:59:07 +0500

    creating promises

---
+++ Promises/promises.js
@@ -0,0 +1,34 @@
+window.onload= function() {
+    function get(url){
+        return new Promise(function(resolve, reject){
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", url, true);
+          xhr.onload = function(){
+            if(xhr.status == 200){
+              resolve(JSON.parse(xhr.response));
+            } else{
+              reject(xhr.statusText);
+            }
+          };
+          xhr.onerror = function(){
+            reject(xhr.statusText);
+          };
+          xhr.send();
+        });
+      }
+
+      var promise = get("data/tweets.json");
+      promise.then(function(tweets){
+          console.log(tweets);
+          return get("data/friends.json");
+      }).then(function(friends){
+          console.log(friends)
+          return get("data/videos.json");
+      }).then(function(videos){
+          console.log(videos)
+      }).catch(function(error){
+            console.log(error)
+      });
+
+
+};
