5adeee81ce9740ef2b2e1ea057057771422a637c
Javeria <javerianadeem03@gmail.com>
2019-06-02 22:45:04 +0500

    making three ajax requests one after another

--- Callback Functions/callback.js
+++ Callback Functions/callback.js
@@ -12,9 +12,49 @@ window.onload = function () {
     // var bodyparts = ['fingers', 'nose', 'face']
     // bodyparts.forEach(callback)
     // console.log("done")
-    $.get('data/tweets.json',function(data){
-        console.log(data)
+
+
+    // $.get('data/tweets.json',function(data){
+    //     console.log(data)
+    // });
+    // console.log("test")
+
+    // //  OR WE CAN DO
+
+    // function cb (data)
+    // {
+    //     console.log(data)
+    // }
+    // $.get('data/friends.json',cb)
+    function handleError(jqXHR, textStatus, error) {
+        console.log(error)
+    }
+    $.ajax({
+        type: "GET",
+        url: "data/tweets.json",
+        success: cbTweets,
+        error: handleError
     });
-    console.log("test")
+    function cbTweets(data) {
+        console.log(data)
+        $.ajax({
+            type: "GET",
+            url: "data/friends.json",
+            success: cbFriends,
+            error: handleError,
+        })
+    }
+    function cbFriends(data) {
+        console.log(data)
+
+        $.ajax({
+            type: "GET",
+            url: "data/videos.json",
+            success: function (data) {
+                console.log(data)
+            },
+            error: handleError
+        })
+    }
 
 };
