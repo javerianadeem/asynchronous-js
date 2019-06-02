2770333f6c117624f63119406558f827c5c02199
Javeria <javerianadeem03@gmail.com>
2019-06-02 22:04:33 +0500

    adding callback functions

---
+++ Callback Functions/callback.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+    // var fruits = ['banana', 'apple', 'peach', 'mango']
+    // fruits.forEach(function (val) {
+    //     console.log(val)
+    // })
+
+    // // OR WE CAN DO
+    // function callback(val) {
+    //     console.log(val)
+    // }
+    // var bodyparts = ['fingers', 'nose', 'face']
+    // bodyparts.forEach(callback)
+    // console.log("done")
+
+
+
+    
+
+};
